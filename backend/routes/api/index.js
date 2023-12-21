d8eda6d03e2be6719c6e96e634318b84843caf6d
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-20 19:46:14 -0500

    Add new files and configurations

---
+++ backend/routes/api/index.js
@@ -0,0 +1,8 @@
+//*====> backend/routes/api/index.js <====
+
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
+
+router.use(restoreUser);
+
+module.exports = router;
