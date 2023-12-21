b6c184b71f20befc78f06817d3aa2aa2d265fff1
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-20 19:45:59 -0500

    Delete unnecessary files and configurations

--- backend/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-//*====> backend/routes/api/index.js <====
-
-const router = require("express").Router();
-const { restoreUser } = require("../../utils/auth.js");
-
-router.use(restoreUser);
-
-module.exports = router;
