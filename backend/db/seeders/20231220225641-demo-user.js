27953baf89f7b12bc3bbcb63df8bfd6decb9a169
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-24 12:42:29 -0500

    Refactor user seeding script

--- backend/db/seeders/20231220225641-demo-user.js
+++ backend/db/seeders/20231220225641-demo-user.js
@@ -1,7 +1,6 @@
 //*====> backend/db/seeders/20231220225641-demo-user.js <====
 "use strict";
 
-const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 const { ValidationError } = require("sequelize");
 
@@ -12,43 +11,79 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await User.bulkCreate(
-      [
-        {
-          firstName: "Peter",
-          lastName: "Parker",
-          email: "spiderman@user.io",
-          username: "SpideyTingles62",
-          hashedPassword: bcrypt.hashSync("password"),
-        },
-        {
-          firstName: "Dwayne",
-          lastName: "Miller",
-          email: "user1@user.io",
-          username: "FakeUser1",
-          hashedPassword: bcrypt.hashSync("password2"),
-        },
-        {
-          firstName: "Paul",
-          lastName: "Miller",
-          email: "user2@user.io",
-          username: "FakeUser2",
-          hashedPassword: bcrypt.hashSync("password3"),
-        },
-      ],
-      { validate: true }
-    ).catch((err) => {
-      if (err instanceof ValidationError) {
-        throw err.message;
-      } else {
-        console.log(err);
-        for (let error of err.errors) {
-          console.log(error.record);
-          console.log(error.message);
+    await queryInterface
+      .bulkInsert(
+        "Users",
+        [
+          {
+            firstName: "Peter",
+            lastName: "Parker",
+            email: "spiderman@marvel.io",
+            username: "SpideyTingles62",
+            hashedPassword: bcrypt.hashSync("SpideyPassword"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            firstName: "Tony",
+            lastName: "Stark",
+            email: "ironman@marvel.io",
+            username: "IamThatDude63",
+            hashedPassword: bcrypt.hashSync("IronManPassword"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            firstName: "Steve",
+            lastName: "Rogers",
+            email: "captainamerica@marvel.io",
+            username: "CaptainSteve40",
+            hashedPassword: bcrypt.hashSync("CaptainPassword"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            firstName: "Bruce",
+            lastName: "Banner",
+            email: "hulk@marvel.io",
+            username: "HulkSmash62",
+            hashedPassword: bcrypt.hashSync("HulkPassword"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            firstName: "Natasha",
+            lastName: "Romanoff",
+            email: "blackwidow@marvel.io",
+            username: "MsStealYoMan64",
+            hashedPassword: bcrypt.hashSync("BlackWidowPassword"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            firstName: "Thor",
+            lastName: "Odinson",
+            email: "thor@marvel.io",
+            username: "GodOfThunder62",
+            hashedPassword: bcrypt.hashSync("MjolnirPassword"),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        options
+      )
+      .catch((err) => {
+        if (err instanceof ValidationError) {
+          throw err.message;
+        } else {
+          console.log(err);
+          for (let error of err.errors) {
+            console.log(error.record);
+            console.log(error.message);
+          }
+          throw err.errors;
         }
-        throw err.errors;
-      }
-    });
+      });
   },
 
   async down(queryInterface, Sequelize) {
@@ -57,7 +92,16 @@ module.exports = {
     return queryInterface.bulkDelete(
       options,
       {
-        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
+        username: {
+          [Op.in]: [
+            "SpideyTingles62",
+            "IamThatDude63",
+            "CaptainSteve40",
+            "HulkSmash62",
+            "MsStealYoMan64",
+            "GodOfThunder62",
+          ],
+        },
       },
       {}
     );
