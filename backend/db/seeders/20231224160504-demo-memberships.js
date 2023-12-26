ec42cb1bcb60adaa10bd924ad839fa3ad33ed9f2
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-26 16:16:55 -0500

    Refactor demo-memberships.js seed file

--- backend/db/seeders/20231224160504-demo-memberships.js
+++ backend/db/seeders/20231224160504-demo-memberships.js
@@ -1,25 +1,59 @@
-'use strict';
+//*====> backend/db/seeders/202312241604XX-demo-memberships.js <====
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    // Bulk inserting data into the Memberships table
+    await queryInterface.bulkInsert(
+      "Memberships",
+      [
+        {
+          userId: 1, // Peter Parker
+          groupId: 1, // The Avengers
+          status: "member",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2, // Tony Stark
+          groupId: 3, // Stark Industries R&D
+          status: "co-host",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3, // Steve Rogers
+          groupId: 5, // Heroes for Justice
+          status: "member",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 4, // Bruce Banner
+          groupId: 2, // Science Bros
+          status: "member",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 5, // Natasha Romanoff
+          groupId: 4, // S.H.I.E.L.D. Espionage
+          status: "co-host",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //^ ... Add more memberships as needed
+      ],
+      options
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Memberships", null, options);
+  },
 };
