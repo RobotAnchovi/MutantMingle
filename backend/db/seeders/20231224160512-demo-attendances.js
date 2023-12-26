981e4a6b019a29c83d2eb8f13d35f3ae7dfac65b
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-26 16:22:01 -0500

    Refactor seed file for Attendances table

--- backend/db/seeders/20231224160512-demo-attendances.js
+++ backend/db/seeders/20231224160512-demo-attendances.js
@@ -1,25 +1,58 @@
-'use strict';
+//*====> backend/db/seeders/20231224160512-demo-attendances.js <====
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Attendances",
+      [
+        {
+          userId: 1, // Peter Parker attending an event
+          eventId: 1, // Corresponds to an event, e.g., 'Avengers Assembly'
+          status: "attending",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2, // Tony Stark attending an event
+          eventId: 3, // Corresponds to 'Stark Expo'
+          status: "attending",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3, // Steve Rogers on the waitlist for an event
+          eventId: 5, // Corresponds to 'Fantastic Four Science Fair'
+          status: "waitlist",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 4, // Bruce Banner pending for an event
+          eventId: 2, // Corresponds to 'S.H.I.E.L.D. Recruitment'
+          status: "pending",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 5, // Natasha Romanoff attending an event
+          eventId: 4, // Corresponds to 'X-Men Training Session'
+          status: "attending",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        //^ ... More entries can be added as needed
+      ],
+      options
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Attendances", null, options);
+  },
 };
