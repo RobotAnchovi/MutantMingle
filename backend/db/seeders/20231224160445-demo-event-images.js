c2b83ba4dccab67949163bf2c9cd5ca7e91e08c5
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-26 16:15:32 -0500

    Refactor event image seed file

--- backend/db/seeders/20231224160445-demo-event-images.js
+++ backend/db/seeders/20231224160445-demo-event-images.js
@@ -1,25 +1,57 @@
-'use strict';
+//*====> backend/db/seeders/20231224160445-demo-event-images.js <====
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "EventImages",
+      [
+        {
+          eventId: 1, // Image for 'Avengers Assembly' event
+          url: "",
+          preview: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          eventId: 2, // Image for 'S.H.I.E.L.D. Recruitment' event
+          url: "",
+          preview: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          eventId: 3, // Image for 'Stark Expo' event
+          url: "",
+          preview: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          eventId: 4, // Image for 'X-Men Training Session' event
+          url: "",
+          preview: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          eventId: 5, // Image for 'Fantastic Four Science Fair' event
+          url: "",
+          preview: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      options
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    // Revert the seed data
+    return queryInterface.bulkDelete("EventImages", null, options);
+  },
 };
