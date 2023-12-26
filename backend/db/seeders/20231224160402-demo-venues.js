2081e598412180124adad480e3cf2b613d27cd30
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-26 14:37:58 -0500

    Add demo venues seed data

--- backend/db/seeders/20231224160402-demo-venues.js
+++ backend/db/seeders/20231224160402-demo-venues.js
@@ -1,6 +1,5 @@
+//*====> backend/db/seeders/<timestamp>-demo-venues.js <====
 "use strict";
-const bcrypt = require("bcryptjs");
-const { ValidationError } = require("sequelize");
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
@@ -9,23 +8,65 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    await queryInterface.bulkInsert(
+      "Venues",
+      [
+        {
+          groupId: 1,
+          address: "890 Fifth Avenue",
+          city: "New York",
+          state: "NY",
+          lat: 40.7746,
+          lng: -73.9653,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 2,
+          address: "177A Bleecker Street",
+          city: "New York",
+          state: "NY",
+          lat: 40.7289,
+          lng: -73.9994,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 3,
+          address: "Xavier's School for Gifted Youngsters",
+          city: "Salem Center",
+          state: "NY",
+          lat: 41.3325,
+          lng: -73.6972,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 4,
+          address: "Stark Tower",
+          city: "New York",
+          state: "NY",
+          lat: 40.7567,
+          lng: -73.9783,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 5,
+          address: "Baxter Building",
+          city: "New York",
+          state: "NY",
+          lat: 40.7564,
+          lng: -73.9832,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      options
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete("Venues", null, {});
   },
 };
