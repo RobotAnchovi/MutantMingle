0b1c3a7743216a17547cdc3b2144106193969f17
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-26 16:06:15 -0500

    Refactor group image seed file

--- backend/db/seeders/20231224160436-demo-group-images.js
+++ backend/db/seeders/20231224160436-demo-group-images.js
@@ -1,25 +1,57 @@
-'use strict';
+//*====> backend/db/seeders/20231224160436-demo-group-images.js <====
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "GroupImages",
+      [
+        {
+          groupId: 1, // Corresponds to 'The Avengers'
+          url: "", // Placeholder for image URL
+          preview: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 2, // Corresponds to 'Science Bros'
+          url: "", // Placeholder for image URL
+          preview: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 3, // Corresponds to 'Stark Industries R&D'
+          url: "", // Placeholder for image URL
+          preview: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 4, // Corresponds to 'S.H.I.E.L.D. Espionage'
+          url: "", // Placeholder for image URL
+          preview: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          groupId: 5, // Corresponds to 'Heroes for Justice'
+          url: "", // Placeholder for image URL
+          preview: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      options
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("GroupImages", null, options);
+  },
 };
