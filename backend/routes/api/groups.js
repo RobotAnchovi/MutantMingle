643ef25f085507cb85bd2c6974ec4940d0501433
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2023-12-27 13:41:25 -0500

    Add venues routes to groups.js

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,7 +1,13 @@
 //*====> backend/routes/api/groups.js <====
 
 const express = require("express");
-const { Group, GroupImage, User, Membership } = require("../../db/models");
+const {
+  Group,
+  GroupImage,
+  User,
+  Membership,
+  Venue,
+} = require("../../db/models");
 const { requireAuth } = require("../../utils/auth");
 const { Sequelize } = require("sequelize");
 
@@ -293,6 +299,75 @@ router.delete("/:groupId", requireAuth, async (req, res, next) => {
 
     return res.json({ message: "Group successfully deleted" });
   } catch (err) {
+    next(err);
+  }
+});
+
+//^ GET route to fetch all venues for a specific group
+router.get("/:groupId/venues", async (req, res, next) => {
+  try {
+    const { groupId } = req.params;
+
+    //* Check if the group exists
+    const groupExists = await Group.findByPk(groupId);
+    if (!groupExists) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+
+    //* Fetch venues associated with the group
+    const venues = await Venue.findAll({
+      where: { groupId: groupId },
+      attributes: ["id", "groupId", "address", "city", "state", "lat", "lng"],
+    });
+
+    return res.json(venues);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//^ POST route to create a new venue for a specific group
+router.post("/:groupId/venues", requireAuth, async (req, res, next) => {
+  try {
+    const { groupId } = req.params;
+    const { address, city, state, lat, lng } = req.body;
+    const userId = req.user.id;
+
+    //* Check if the group exists and if the user is authorized (owner or co-host)
+    const group = await Group.findByPk(groupId);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    if (group.organizerId !== userId) {
+      const membership = await Membership.findOne({
+        where: { groupId, userId, status: "co-host" },
+      });
+      if (!membership) {
+        return res
+          .status(403)
+          .json({
+            message: "User is not authorized to create a venue for this group",
+          });
+      }
+    }
+
+    //* Create the new venue
+    const newVenue = await Venue.create({
+      groupId,
+      address,
+      city,
+      state,
+      lat,
+      lng,
+    });
+
+    return res.status(201).json(newVenue);
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res
+        .status(400)
+        .json({ message: "Validation error", errors: err.errors });
+    }
     next(err);
   }
 });
