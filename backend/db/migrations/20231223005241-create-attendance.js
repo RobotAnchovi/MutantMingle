77a2bb8c3cfb1797f797bc3dcca7bb75670a4ad6
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-03 14:51:49 -0500

    Refactor table name in create-attendance migration

--- backend/db/migrations/20231223005241-create-attendance.js
+++ backend/db/migrations/20231223005241-create-attendance.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(
-      "Attendances",
+      "Attendance",
       {
         id: {
           allowNull: false,
