602c7bc6befb16e810fa4b2b7082be434b72c7a2
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-07 21:10:04 -0500

    Update error message in session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,7 +39,7 @@ router.post("/", validateLogin, async (req, res, next) => {
     const err = new Error("Login failed");
     err.status = 401;
     err.title = "Login failed";
-    err.errors = { credential: "The provided credentials were invalid." };
+    err.errors = { message: "The provided credentials were invalid." };
     return next(err);
   }
 
