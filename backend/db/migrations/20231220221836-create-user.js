58775d569c30bf4e3fd47f3eeda2338528948cea
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-19 17:25:44 -0500

    Add new files and configurations

---
+++ backend/db/migrations/20231220221836-create-user.js
@@ -0,0 +1,60 @@
+//*====> backend/db/migrations/20231220221836-create-user.js <====
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "Users",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        firstName: {
+          type: Sequelize.STRING(50),
+        },
+        lastName: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(256),
+          allowNull: false,
+          unique: true,
+        },
+        username: {
+          type: Sequelize.STRING(30),
+          allowNull: false,
+          unique: true,
+        },
+        hashedPassword: {
+          type: Sequelize.STRING.BINARY,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+      },
+      options
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
+  },
+};
