aa87df2a3bb24b39676ba8878d60536e06cf583d
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-19 18:32:59 -0500

    Comment out XSRF-TOKEN cookie creation in production

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -30,14 +30,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-//~====> Add a XSRF-TOKEN cookie <====
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
+// //~====> Add a XSRF-TOKEN cookie <====
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     "XSRF-Token": csrfToken,
+//   });
+// });
 
 //~====> Add XSRF-TOKEN cookie DEVELOPMENT <====
 if (process.env.NODE_ENV !== "production") {
