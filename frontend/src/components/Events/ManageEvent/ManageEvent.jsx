2a05351c26c24654ba5e30a30f1761ed71020e60
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-20 17:17:58 -0500

    Delete unused files and CSS rules

--- frontend/src/components/Events/ManageEvent/ManageEvent.jsx
+++
@@ -1,7 +0,0 @@
-import "./ManageEvent.css";
-
-const ManageEvent = () => {
-  return null;
-};
-
-export default ManageEvent;
--- frontend/src/components/Groups/ListGroupItem/index.js
+++
@@ -1,3 +0,0 @@
-import ListGroupItem from "./ListGroupItem";
-
-export default ListGroupItem;
