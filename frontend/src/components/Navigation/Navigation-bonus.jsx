6edd3d17bd74e7c6a4f6a6114973fceec9d9e63a
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-21 12:03:41 -0500

    Update Navigation component and remove unused files

--- frontend/src/components/Navigation/Navigation-bonus.jsx
+++ frontend/src/components/Navigation/Navigation-bonus.jsx
@@ -1,29 +1,29 @@
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton-bonus";
-import "./Navigation.css";
+// import { NavLink } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import ProfileButton from "./ProfileButton-bonus";
+// import "./Navigation.css";
 
-function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector((state) => state.session.user);
 
-  return (
-    <div className="navigation-container">
-      <ul className="nav-list">
-        <li>
-          <NavLink to="/">
-            <img src={"#"} alt="Offline" />
-          </NavLink>
-        </li>
-      </ul>
-      <ul className="nav-list">
-        {isLoaded && (
-          <li>
-            <ProfileButton user={sessionUser} />
-          </li>
-        )}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div className="navigation-container">
+//       <ul className="nav-list">
+//         <li>
+//           <NavLink to="/">
+//             <img src={"#"} alt="Offline" />
+//           </NavLink>
+//         </li>
+//       </ul>
+//       <ul className="nav-list">
+//         {isLoaded && (
+//           <li>
+//             <ProfileButton user={sessionUser} />
+//           </li>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default Navigation;
+// export default Navigation;
--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,44 +1,81 @@
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import { Link } from "react-router-dom";
+import cyclopsLogo from "../../../dist/cyclops copy.png";
 import "./Navigation.css";
 
-function Navigation({ isLoaded }) {
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector((state) => state.session.user);
+
+//   const sessionLinks = sessionUser ? (
+//     <li>
+//       <ProfileButton user={sessionUser} />
+//     </li>
+//   ) : (
+//     <>
+//       <li>
+//         <OpenModalButton
+//           buttonText="Enter your Faction"
+//           modalComponent={<LoginFormModal />}
+//         />
+//         {/* <NavLink to="/login">Log In</NavLink> */}
+//       </li>
+//       <li>
+//         <OpenModalButton
+//           buttonText="Enlist Today!"
+//           modalComponent={<SignupFormModal />}
+//         />
+//         {/* <NavLink to="/signup">Sign Up</NavLink> */}
+//       </li>
+//     </>
+//   );
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+//       {isLoaded && sessionLinks}
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
+
+function Navigation() {
   const sessionUser = useSelector((state) => state.session.user);
-
-  const sessionLinks = sessionUser ? (
-    <li>
-      <ProfileButton user={sessionUser} />
-    </li>
-  ) : (
-    <>
-      <li>
-        <OpenModalButton
-          buttonText="Enter your Faction"
-          modalComponent={<LoginFormModal />}
-        />
-        {/* <NavLink to="/login">Log In</NavLink> */}
-      </li>
-      <li>
-        <OpenModalButton
-          buttonText="Enlist Today!"
-          modalComponent={<SignupFormModal />}
-        />
-        {/* <NavLink to="/signup">Sign Up</NavLink> */}
-      </li>
-    </>
-  );
-
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && sessionLinks}
-    </ul>
+    <nav>
+      <div className="nav-bar-logo">
+        <Link to="/">
+          <img id="logo" src={cyclopsLogo} alt="Home" />
+        </Link>
+      </div>
+
+      {sessionUser ? (
+        <div className="nav-bar-user-links">
+          <Link to={"/groups/new"} className="link">
+            Start a new faction!
+          </Link>
+          <ProfileButton user={sessionUser} />
+        </div>
+      ) : (
+        <div className="nav-bar-no-user">
+          <OpenModalButton
+            buttonText="Enter your Portal"
+            modalComponent={<LoginFormModal />}
+          />
+          <OpenModalButton
+            buttonText="Enlist Today!"
+            modalComponent={<SignupFormModal />}
+          />
+        </div>
+      )}
+    </nav>
   );
 }
 
