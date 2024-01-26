6682cdaf31fd70cb9ad64cc5d4398cf37ee60b42
RobotAnchovi <140855478+RobotAnchovi@users.noreply.github.com>
2024-01-26 12:07:43 -0500

    Update path to frontend index.html

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.sendFile(
-      path.resolve(__dirname, "../../", "dist", "index.html")
+      path.resolve(__dirname, "../../frontend", "dist", "index.html")
     );
   });
 }
